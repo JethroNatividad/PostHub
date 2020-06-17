6c80779d2a21bee486de6f845646cbc3954d9dcc
jethrosama <you@example.com>
2020-06-17 19:55:09 +0800

    add user authentication, login, signup ,logout

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ bodyParser             = require("body-parser"),
 mongoose               = require("mongoose"),
 Post                   = require("./models/post"),
 Comment                = require("./models/comment"),
+User                   = require("./models/user"),
 postsRoute             = require("./routes/posts"),
 commentsRoute          = require("./routes/comments"),
-indexRoute             = require("./routes/comments"),
+indexRoute             = require("./routes/index"),
 methodOverride         = require("method-override"),
+passport               = require("passport"),
+localStrategy          = require("passport-local"),
+passportLocalMongoose  = require("passport-local-mongoose"),
+expressSession         = require("express-session"),
 seedDB                 = require("./seed"),
 app                    = express();
 let port = 3000;
@@ -21,6 +26,25 @@ mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 mongoose.connect("mongodb+srv://jethrosama:undeadban07@master-2viyl.mongodb.net/posthublocal?retryWrites=true&w=majority");
+//Passport Auth setup
+app.use(expressSession({
+    secret: "nicenicenicenicenice",
+    resave: false,
+    saveUninitialized: false
+  }));
+  
+  passport.use(new localStrategy(User.authenticate()));
+  
+  app.use(passport.initialize());
+  app.use(passport.session());
+  
+  passport.serializeUser(User.serializeUser());
+  passport.deserializeUser(User.deserializeUser());
+  //data of current user pass in all routes
+    app.use((req , res, next)=>{
+    res.locals.currentUser = req.user;
+    next();
+  });
 //seedDB(); //seeder
 //routes
 app.use("/", indexRoute)
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,46 @@
-const express          = require("express"),
+const passport         = require("passport"),
+express                = require("express"),
 Post                   = require("../models/post"),
+User                   = require("../models/user"),
 Comment                = require("../models/comment"),
 router                 = express.Router()
 //landing page
 router.get("/", (req, res)=>{
     res.render("landing");
 });
+//render sign up page
+router.get("/signup", (req, res)=>{
+    res.render("index/signup")
+})
+//handle signup logic
+router.post("/signup", (req, res)=>{
+    var newUser = {
+        username: req.body.username
+    }
+    User.register(newUser, req.body.password, (err, user)=>{
+        if (err) {
+            console.log(err)
+            return res.redirect("/posts")
+        }
+        passport.authenticate("local")(req, res, ()=>{
+            console.log(user)
+            res.redirect("/posts")
+        })
+    })
+})
+//render login page
+router.get("/login", (req, res)=>{
+    res.render("index/login")
+})
+//handle login logic
+router.post("/login", passport.authenticate("local", {
+    successRedirect: "/posts",
+    failureRedirect: "/login"
+}), (req, res)=>{
+})
+//logout logic
+router.get("/logout", (req, res)=>{
+    req.logout()
+    res.redirect("/")
+})
 module.exports = router
